d47f84759acf931ab4354793e392f1cbeeefa20b
renanrcs <renan.cavalcante@yahoo.com>
2023-04-01 19:49:56 -0300

    projeto api

---
+++ src/main/java/br/com/renanrcs/linguagens/api/Linguagem.java
@@ -0,0 +1,38 @@
+package br.com.renanrcs.linguagens.api;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "principaisLinguagens")
+public class Linguagem {
+
+    @Id
+    private String id;
+    private String title;
+    private String image;
+    private String ranking;
+
+    public Linguagem(String title, String image, String ranking) {
+        this.title = title;
+        this.image = image;
+        this.ranking = ranking;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getRanking() {
+        return ranking;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    
+}
